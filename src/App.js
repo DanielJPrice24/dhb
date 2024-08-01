909164ef8dc6451ec75faf13f5cd0bad3e71c86b
unknown <daniel.price2244@gmail.com>
2024-08-01 12:42:04 -0400

    vercel speed insights

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Footer from './footer/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
+      <SpeedInsights />
       <Header />
       <Main />
       <Footer />
-      <Analytics />
+      
     </div>
   );
 }
