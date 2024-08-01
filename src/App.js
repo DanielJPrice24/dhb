a453e1eccef1fd6d253911b538874d9341829545
unknown <daniel.price2244@gmail.com>
2024-08-01 12:55:17 -0400

    remove speed insights

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import Main from './main/main'
 import Footer from './footer/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+
 
 function App() {
   return (
     <div className="App">
-      <Analytics />
-      <SpeedInsights />
       <Header />
       <Main />
       <Footer />
-      
+      <Analytics />
     </div>
   );
 }
